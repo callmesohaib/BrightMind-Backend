d1da05eea53c8acb2fc771dd33c169aa25a6b934
NO ONE <sohaibikram249@gmail.com>
2024-09-28 11:33:19 +0500

    update

--- api/index.js
+++ api/index.js
@@ -16,6 +16,11 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/data", courseRouter);
 
+app.use('/', function(req, res) {
+  res.send('Hello from the server!');
+});
+
+
 const port = 3000;
 
 connectDb().then(() => {
