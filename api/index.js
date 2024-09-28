3501afcd37dec2ec9d9c71ae32aa853d30ea8c9f
NO ONE <sohaibikram249@gmail.com>
2024-09-28 11:18:15 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
-const cors= require("cors")
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const authRouter = require("./Routes/auth-router");
-const connectDb = require("./Utils/db");
-const courseRouter = require("./Routes/course-router");
+const authRouter = require("../Routes/auth-router");
+const connectDb = require("../Utils/db");
+const courseRouter = require("../Routes/course-router");
 
 const Corsoptions = {
   origin: ["http://localhost:5173"],
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
   credentials: true,
 };
-app.use(cors(Corsoptions))
+app.use(cors(Corsoptions));
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/data", courseRouter);
@@ -23,4 +23,3 @@ connectDb().then(() => {
     console.log("Server running on port 3000");
   });
 });
-
